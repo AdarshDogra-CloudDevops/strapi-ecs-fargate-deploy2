8d73ffe1c1b0b0cf2aa02dcae4e4346249351d63
Ubuntu <ubuntu@ip-172-31-17-252.eu-north-1.compute.internal>
2025-06-26 07:45:15 +0000

    Policy updated for workflow

--- alb.tf
+++ alb.tf
@@ -10,24 +10,7 @@ resource "aws_lb" "strapi" {
   }
 }
 
-resource "aws_lb_target_group" "strapi" {
-  name         = "${var.project_name}-tg"
-  port         = 1337
-  protocol     = "HTTP"
-  vpc_id       = aws_vpc.main.id
-  target_type  = "ip"
-
-  health_check {
-    path                = "/admin"
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    matcher             = "200-399"
-  }
-}
-
-# ✅ Blue Target Group
+# 🔵 Blue Target Group
 resource "aws_lb_target_group" "blue" {
   name         = "strapi-blue-tg"
   port         = 1337
@@ -45,7 +28,7 @@ resource "aws_lb_target_group" "blue" {
   }
 }
 
-# ✅ Green Target Group
+# 🟢 Green Target Group
 resource "aws_lb_target_group" "green" {
   name         = "strapi-green-tg"
   port         = 1337
@@ -63,6 +46,7 @@ resource "aws_lb_target_group" "green" {
   }
 }
 
+# 🔊 Listener (initially forwarding to Blue)
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.strapi.arn
   port              = 80
@@ -70,6 +54,6 @@ resource "aws_lb_listener" "http" {
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.strapi.arn
+    target_group_arn = aws_lb_target_group.blue.arn
   }
 }
