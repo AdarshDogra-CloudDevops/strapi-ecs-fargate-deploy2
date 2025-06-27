245dd3c99371c19ff12aecd5893f613350845a0f
Ubuntu <ubuntu@ip-172-31-17-252.eu-north-1.compute.internal>
2025-06-27 05:08:13 +0000

    Fix: Use Terraform output for ECS Task Definition ARN

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,6 @@ output "alb_dns_name" {
   description = "Public URL of the Application Load Balancer"
   value       = aws_lb.strapi.dns_name
 }
+output "task_definition_arn" {
+  value = aws_ecs_task_definition.strapi.arn
+}
